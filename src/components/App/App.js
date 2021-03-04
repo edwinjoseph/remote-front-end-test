9a5c4f49d86c246466c2d2b43190d6af18d39711
Edwin Joseph <edwin@wonderbly.com>
2021-03-04 19:04:51 +0000

    feat(PropertyListing): connect app to properties api and fetch on mount

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import './App.scss';
+import React, { useState, useEffect } from 'react';
+import fetch from 'cross-fetch';
 import Header from '../Header';
 import SortAndFilter from '../SortAndFilter';
 import PropertyListing from '../PropertyListing';
+import './App.scss';
 
 const App = () => {
+    const [ properties, setProperties ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+    const [ error, setError ] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await fetch('http://localhost:3000/api/properties');
+                const data = await res.json();
+                setProperties(data);
+            } catch (err) {
+                console.log('Error: Failed to fetch properties', err);
+                setError(err);
+            }
+            setLoading(false);
+        })();
+    });
+
     return (
         <div className="App">
             <Header/>
             <main>
                 <SortAndFilter/>
-                <PropertyListing/>
+                <PropertyListing loading={loading} items={properties} error={error} />
             </main>
         </div>
     );
